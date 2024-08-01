4ba9d3fb3decf53c2f9ef98414b98be0ea5b9402
Satyam Kumar <130126922+satyam26en@users.noreply.github.com>
2024-08-01 23:14:05 +0530

    Update result_1.py

--- result_1.py
+++ result_1.py
@@ -12,7 +12,7 @@ import pandas as pd
 import plotly.express as px
 
 # Streamlit app title
-st.title('Average Salary and Job Openings by Experience Category')
+st.title('Average Salary and Number of Job Openings ')
 
 # Load the dataset from the provided URL
 file_url = "https://raw.githubusercontent.com/satyam26en/JOB/main/Clean_Job_File.csv"
